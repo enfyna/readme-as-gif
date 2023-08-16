1fdb580acb3a4ed96f8a2b0b653369b957617cb4
enfyna <legendraslan@gmail.com>
2023-08-16 16:14:50 +0300

    Add progress bar

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,67 +1,81 @@
 let current_form;
 let base_form_data;
-let game_form_data;
+let game_form_data = '';
+let selected_game = 'base';
+
+const result = document.getElementById('result');
 
 window.addEventListener('DOMContentLoaded',()=>{
-	document.getElementById('generate').onclick = generateBase;
+	document.getElementById('generate').onclick = generate;
 	document.getElementById('next').onclick = get_game_form;
 
 	current_form = document.getElementById('baseForm');
 });
 
-
-function generateBase() {
+function generate(){
 	if(current_form == null){
 		return;
 	}
-	base_form_data = new FormData(current_form);
+	if(selected_game == 'base'){
+		base_form_data = get_form_as_url_params(current_form);
+	}
+	else{
+		game_form_data = get_form_as_url_params(current_form);
+	}
 
-	const keysToDelete = [];
-
-	base_form_data.forEach((val, key) => {
-		if (key.endsWith('color')) {
-			base_form_data.set(key, rgb2bgr(base_form_data.get(key).slice(1)));
-		} else if (val.trim().length === 0) {
-			keysToDelete.push(key);
-		}
-	});
-
-	keysToDelete.forEach(key => base_form_data.delete(key));
-
-	const url = '/api/base?' + new URLSearchParams(base_form_data).toString();
-	const gifImage = document.getElementById('gifImage');
-	gifImage.src = url;
+	const url = '/api/'.concat(selected_game,'?',base_form_data,'&',game_form_data);
+	fetchGIF(url);
 }
 
 function get_game_form() {
-	if(base_form_data == null){
+	if(base_form_data == null || selected_game == 'base'){
 		return;
 	}
 	current_form.hidden = true;
-	current_form = document.getElementById(base_form_data.get('game'));
-	document.getElementById('generate_'+base_form_data.get('game')).onclick = generateGame;
+	current_form = document.getElementById(selected_game);
+	document.getElementById('generate_'+selected_game).onclick = generate;
 	current_form.hidden = false;
 }
 
-function generateGame(){
-	game_form_data = new FormData(current_form);
+function fetchGIF(url){
+	const progress_bar = document.getElementById('progress')
+	progress_bar.style.width = '0%';
+	
+	let req = new XMLHttpRequest();
+	req.onreadystatechange = () => {
+		if(req.readyState == 4 && req.status == 200){
+			result.src = req.responseURL;
+		}
+	}
+	req.onprogress = (e) => {
+		if(e.lengthComputable){
+			progress_bar.style.width =	(e.loaded / e.total * 100).toString()+'%'
+		}
+	}
+	req.open('GET',url);
+	req.send();
+}
+
+function get_form_as_url_params(form) {
+	const formData = new FormData(form);
 
 	const keysToDelete = [];
 
-	game_form_data.forEach((val, key)=>{
-		if(key.endsWith('color')){
-			game_form_data.set(key, rgb2bgr(game_form_data.get(key).slice(1)));
+	formData.forEach((val, key) => {
+		if (key.endsWith('color')) {
+			formData.set(key, rgb2bgr(formData.get(key).slice(1)));
+		}
+		else if(key == 'game'){
+			selected_game = val;
 		}
 		else if (val.trim().length === 0) {
 			keysToDelete.push(key);
 		}
 	});
 
-	keysToDelete.forEach(key => base_form_data.delete(key));
+	keysToDelete.forEach(key => formData.delete(key));
 
-	const url = '/api/' + base_form_data.get('game') + '?' + new URLSearchParams(game_form_data).toString() + '&' +new URLSearchParams(base_form_data).toString();
-	const gifImage = document.getElementById('gifImage');
-	gifImage.src = url;
+	return new URLSearchParams(formData).toString();
 }
 
 function rgb2bgr(rgb){
